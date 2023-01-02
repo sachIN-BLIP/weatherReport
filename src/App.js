83954a381cc2bcf1f1d1804eed1b2788efee0e18
Sachin Yadav <55685345+sachIN-BLIP@users.noreply.github.com>
2023-01-02 18:19:50 +0530

    filesAdded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import Temp from "./components/temp"
+
+function App() {
+  return (
+    <>
+    <Temp/>
+    </>
+  );
+}
+
+export default App;
